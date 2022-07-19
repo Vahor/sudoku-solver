0519be61a5d14600a8c626ffce77d32a58cef531
vahor <me@vahor.fr>
2022-07-19 20:00:55 +0200

    replace googleanalytics with unami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 // src/pages/_app.tsx
 import type { AppType } from "next/dist/shared/lib/utils";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <GoogleAnalytics />
+      <script async defer data-website-id="4752123b-6e48-4d37-80b7-ce8d551ef168" src="https://analytics.vahor.fr/umami.js"></script>
       <Component {...pageProps} />
     </>)
 };
