d9776e10a9b228c3aaad09adf7bec722d7e9d2ab
Vahor <me@vahor.fr>
2024-04-20 18:15:09 +0200

    remove analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import "../styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
-      <script async defer data-website-id="4752123b-6e48-4d37-80b7-ce8d551ef168" src="https://analytics.vahor.fr/umami.js"></script>
       <Component {...pageProps} />
     </>)
 };
